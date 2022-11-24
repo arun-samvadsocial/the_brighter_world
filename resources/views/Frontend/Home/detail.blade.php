899941e541f8b2ac63972b64a3f0ca86c02f15f8
arun-samvadsocial <arun.vishwakarma@samvadsocial.com>
2022-11-24 11:04:21 +0530

    ip

--- resources/views/Frontend/Home/detail.blade.php
+++ resources/views/Frontend/Home/detail.blade.php
@@ -6,7 +6,8 @@ $device_ip = Helper::getClientIps();
 
 $minutes = 60;
 Cookie::queue('device_ip', $device_ip, $minutes);
-if(Cookie::get('device_ip') != $device_ip){
+Cookie::queue('post_id', $post_data->post_id, $minutes);
+if(Cookie::get('device_ip') != $device_ip $post_data->post_id != Cookie::get('post_id')){
 isset($post_data)?Helper::updateViewCount($post_data->post_id):'';
 }
 $link = url('detail/'.$post_data->post_url.'/'.Helper::base64url_encode($post_data->post_id));
