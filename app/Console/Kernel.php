2ef47ca3e7ca35f75f39a4556c8180261c90402e
arun-samvadsocial <arun.vishwakarma@samvadsocial.com>
2022-12-08 18:16:31 +0530

    set command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('checkpostcron')->daily();
+        $schedule->command('checkpostcron')->everyTwoMinutes();
     }
 
     /**
