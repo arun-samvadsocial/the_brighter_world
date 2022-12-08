deab6febee215185d194383e22e889971901b562
arun-samvadsocial <arun.vishwakarma@samvadsocial.com>
2022-12-08 14:50:41 +0530

    scheduled post

--- app/Http/Controllers/API/CronController.php
+++ app/Http/Controllers/API/CronController.php
@@ -10,19 +10,24 @@ use App\Models\Admin\Posts_model;
 class CronController extends Controller
 {
     public function checkScheduledPost(){
-        $posts = Posts_model::where("scheduled_status", 3)->get();
-        foreach($posts as $row){
-            $published_date = date('Y-m-d', strtotime($row->published_date));
-            $today_date = date('Y-m-d');
-            $today_date = strtotime($today_date);
-            $published_date = strtotime($published_date);
-            if ($today_date == $published_date){
-                Posts_model::where("scheduled_status", 3)
-                ->where("post_id",$row->post_id)
-                ->update([
-                    "status"=>1
-                ]);
+        try{
+            $posts = Posts_model::where("scheduled_status", 3)->get();
+            foreach($posts as $row){
+                $published_date = date('Y-m-d', strtotime($row->published_date));
+                $today_date = date('Y-m-d');
+                $today_date = strtotime($today_date);
+                $published_date = strtotime($published_date);
+                if ($today_date == $published_date){
+                    Posts_model::where("scheduled_status", 3)
+                    ->where("post_id",$row->post_id)
+                    ->update([
+                        "status"=>1
+                    ]);
+                }
             }
+        }catch(\Exception $exception){
+            // dd($exception);
+            echo "Something went wrong";
         }
     }
 }
